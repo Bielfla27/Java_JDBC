cc7ad0fd5c96972e43217688f7e4b2cd65342380
Bielfla27 <gabibielmft@outlook.com>
2022-09-12 21:39:07 -0300

    assumindo o controle de quando vai ser commitado a inserção

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -7,29 +7,34 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "Monitor";
-		String descricao = "AOC 27 FHD";
+		
 		
 		CriaConexao criarConexao = new CriaConexao();
 		Connection connection = criarConexao.recuperarConecao();
-		
-		
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)"
-				, Statement.RETURN_GENERATED_KEYS);
-		stm.setString(1, nome);
-		stm.setString(2, descricao);
-		stm.execute();
+		connection.setAutoCommit(false); // com isso eu estou assumindo o controle e dizendo que só vou commita quando eu quiser
 		
 		//PreparedStatement ele deixa o código mais seguro e trás a responsabilidade da montagem da query para ele
 		// Uma das vantagens em se utilizar o PreparedStatement é que o mesmo evita o SQL injection.
 		//O PreparedStatement mantém a consulta compilada no banco de dados, para o usuário que necessitar realizar a mesma consulta, diversas vezes, com parâmetros diferentes.
+		
+		
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)"
+				, Statement.RETURN_GENERATED_KEYS);
+		adicionarVariavel("SmartTV", "50 Polegadas", stm);
+		adicionarVariavel("Controle", "Controle Xbox", stm);
+		
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		stm.execute();
 		
 		ResultSet rst = stm.getGeneratedKeys(); 
 		while(rst.next()) {
 			Integer id = rst.getInt(1);
 			System.out.println("O id doi: " + id);
 		}
-		
 	}
 
 }
