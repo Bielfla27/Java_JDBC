d4c0f888dd28481d4e50fe326a01095720bc260e
Bielfla27 <gabibielmft@outlook.com>
2022-09-12 21:58:07 -0300

    Tratando fechamentos de conexões com o try

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -10,38 +10,37 @@ public class TestaInsercaoComParametro {
 		
 		
 		CriaConexao criarConexao = new CriaConexao();
-		Connection connection = criarConexao.recuperarConecao();
-		connection.setAutoCommit(false); // com isso eu estou assumindo o controle e dizendo que só vou commita quando eu quiser
-		
-		//PreparedStatement ele deixa o código mais seguro e trás a responsabilidade da montagem da query para ele
-		// Uma das vantagens em se utilizar o PreparedStatement é que o mesmo evita o SQL injection.
-		//O PreparedStatement mantém a consulta compilada no banco de dados, para o usuário que necessitar realizar a mesma consulta, diversas vezes, com parâmetros diferentes.
-		
-		try {
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)"
-					, Statement.RETURN_GENERATED_KEYS);
-			adicionarVariavel("SmartTV", "50 Polegadas", stm);
-			adicionarVariavel("Controle", "Controle Xbox", stm);
-			connection.commit();
-			stm.close();	
-		}catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROOLBACK EXECUTADO");
-			connection.rollback();
-		}finally {
-			connection.close();
+			try(Connection connection = criarConexao.recuperarConecao()){
+			connection.setAutoCommit(false); // com isso eu estou assumindo o controle e dizendo que só vou commita quando eu quiser
+			
+			//PreparedStatement ele deixa o código mais seguro e trás a responsabilidade da montagem da query para ele
+			// Uma das vantagens em se utilizar o PreparedStatement é que o mesmo evita o SQL injection.
+			//O PreparedStatement mantém a consulta compilada no banco de dados, para o usuário que necessitar realizar a mesma consulta, diversas vezes, com parâmetros diferentes.
+			
+			try(PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)"
+						, Statement.RETURN_GENERATED_KEYS)){
+				adicionarVariavel("SmartTV", "50 Polegadas", stm);
+				adicionarVariavel("Controle", "Controle Xbox", stm);
+				connection.commit();
+				stm.close();	
+			}catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
+		
 	}	
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 		stm.execute();
 		
-		ResultSet rst = stm.getGeneratedKeys(); 
-		while(rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id doi: " + id);
+		try(ResultSet rst = stm.getGeneratedKeys()){ 
+			while(rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id doi: " + id);
+			}
 		}
 	}
-
 }
